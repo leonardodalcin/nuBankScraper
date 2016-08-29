167d90c81911090a0e0a74e9a0e1629efd0e3e27
Leonardo Dalcin <leon.dalcin@gmail.com>
2016-08-28 23:47:00 -0300

    refactored /scrape in server

--- server.js
+++ server.js
@@ -22,27 +22,32 @@ app.use( '/bower_components', express.static( __dirname + '/bower_components' ))
 
 
 app.get('/scrape', function(req, res){
-  const Browser = require('zombie');
-
+  var Browser = require('zombie');
+  // var user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20';
+  // var browser = new Browser({userAgent: user_agent, debug: true, waitFor: 10000});
   var url = 'https://conta.nubank.com.br/#/login'
-  Browser.visit(url, function(error, browser) {
-    console.log(1)
-    setTimeout(function(){
+
+  var login = '';
+  var password = '';
+
+  var browser = new Browser();
+  browser.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20';
+
+  browser.visit(url, function(error) {
+    setTimeout(function () {
+      console.log(error)
+      console.log('were in')
       browser.assert.text('title', 'Login - Nubank');
       browser
-        .fill('input[id=username]',  '')
-        .fill('input[id=input_001]', '')
-        .pressButton('entrar');
-        browser.dump();
-        setTimeout(function(){
-          console.log('gonnaRoll')
-          browser.assert.text('title', 'Transações - Nubank');
-        }, 60*1000);
-    }, 20*1000);
-
-  });
-
-
+        .fill('input[id=username]',  login)
+        .fill('input[id=input_001]', password)
+        .pressButton('entrar')
+      browser.wait().then(function() {
+        console.log('gonnaRoll')
+        browser.assert.text('title', 'Transações - Nubank');
+      })
+    }, 30000)
+  })
 })
 
 app.listen( 3000, function() {
